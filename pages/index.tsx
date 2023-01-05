37f04bea6df5ed9dce34021a760ef2383de81a18
vy.ct <vyct@mor.com.vn>
2023-01-05 13:15:44 +0700

    Create basic building blocks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
+import type { NextPage } from "next"
+// Components
 import Header from "../components/Header/Header"
+import Hero from "../components/Hero/Hero"
+import Grid from "../components/Grid/Grid"
+import Card from "../components/Card/Card"
+import Spinner from "../components/Spinner/Spinner"
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <main className="relative h-screen overflow-y-scroll">
       <Header />
+      <Hero />
+      <Grid />
+      <Card />
+      <Spinner />
     </main>
   )
 }
+
+export default Home;

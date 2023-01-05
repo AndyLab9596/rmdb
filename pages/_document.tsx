1beced5af0fe1ef6be559d8b9eb199fccc1f133a
vy.ct <vyct@mor.com.vn>
2023-01-05 11:13:38 +0700

    Setup TailwindCSS

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head >
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet"></link>
+      </Head>
+      <body className='font-raleway'>
         <Main />
         <NextScript />
       </body>
